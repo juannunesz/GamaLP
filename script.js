08a4c9ea4f1f2d93f35a19704336f7baf468660e
khale-landvoigt <art.khale@gmail.com>
2023-10-30 22:49:24 -0300

    menu

---
+++ script.js
@@ -0,0 +1,4 @@
+const hamburger = document.querySelector(".hamburger");
+const header = document.querySelector(".header");
+
+hamburger.addEventListener("click", () => header.classList.toggle("active"));
