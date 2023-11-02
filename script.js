aab968815ff28bc83b3669c33e0125aa3b481486
khale-landvoigt <art.khale@gmail.com>
2023-11-02 05:26:55 -0300

    basic final mobile with corrections

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const hamburger = document.querySelector(".hamburger");
 const header = document.querySelector(".header");
 
 hamburger.addEventListener("click", () => header.classList.toggle("active"));
+
+// hamburger.addEventListener("click", () => [
+//     console.log ("teste")
+// ])
