f0eab8f0b581633a9460e81970d2ab95bce9cfbd
juannunesweb <juan.nunes@webrota.com.br>
2023-11-10 18:56:55 +0000

    add anchor links

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ hamburger.addEventListener("click", () => header.classList.toggle("active"));
 // hamburger.addEventListener("click", () => [
 //     console.log ("teste")
 // ])
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+  
+      if (targetSection) {
+        window.scrollTo({
+          behavior: 'smooth',
+          top: targetSection.offsetTop,
+        });
+      }
+    });
+});
